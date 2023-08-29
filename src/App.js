1764296b92143bea885728a14997afb440db0c59
Sahil Kharya <kharyasahil@gmail.com>
2023-08-29 15:36:15 -0700

    Puzzle Wallet Provider Fix

--- src/App.js
+++ src/App.js
@@ -8,70 +8,28 @@ import { Route, Routes } from "react-router-dom";
 import {
   useAccount,
   useConnect,
-  useExecuteProgram,
-  PuzzleWalletProvider,
 } from '@puzzlehq/sdk';
-import { PuzzleWeb3Modal } from '@puzzlehq/sdk';
 import Activity from "./components/Activity";
 
 function App() {
   const { connect } = useConnect();
-  const { account, accounts, isConnected } = useAccount();
-  const {
-    execute,
-    loading,
-    transactionId,
-    outputPrivate,
-    outputRecords,
-    outputPublic,
-    outputConstant,
-    error,
-  } = useExecuteProgram({
-    programId: "rfq_v000003.aleo",
-    functionName: 'create_token',
-    // Aleo program inputs need their types specified, our program takes in 32 bit integers
-    // so the inputs should look like "2i32 3i32"
-    inputs: "11u64 " + "8u8 " + "20u64",
-  });
+  const { account, isConnected } = useAccount();
 
   return (
     <>
-      <PuzzleWalletProvider>
-        <div className="App">
-          <Header connect={connect} isConnected={isConnected} address={account} />
-
-          <div className="mainWindow">
-
-            <Routes>
-              <Route path='/' element={<Home isConnected={isConnected} connect={connect} />} />
-              <Route path='/swap' element={<Swap isConnected={isConnected} address={account}/>} />
-              <Route path='/mint' element={<Mint />} />
-              <Route path='/create' element={<Create />} />
-              <Route path='/activity' element={<Activity />} />
-            </Routes>
-          </div>
-          {/* <div>
-            <button
-              onClick={() => execute()}
-              disabled={loading}
-            >
-              execute program
-            </button>
-            {error && <p>error executing program: {error}</p>}
-            {loading && <p>executing program...</p>}
-            {transactionId && !loading && !error && <p>Transaction Id: {transactionId}</p>}
-            {outputPrivate && (
-              <p>{"Result:" + outputPrivate.replace("i32", "")}</p>
-            )}
-          </div> */}
+      <div className="App">
+        <Header connect={connect} isConnected={isConnected} address={account} />
+        <div className="mainWindow">
+          <Routes>
+            <Route path='/' element={<Home isConnected={isConnected} connect={connect} />} />
+            <Route path='/swap' element={<Swap isConnected={isConnected} address={account} />} />
+            <Route path='/mint' element={<Mint />} />
+            <Route path='/create' element={<Create />} />
+            <Route path='/activity' element={<Activity />} />
+          </Routes>
         </div>
-      </PuzzleWalletProvider>
-      <PuzzleWeb3Modal
-        dAppName='Puzzle Starter app'
-        dAppDescription="Let's Puzzle!"
-        dAppUrl='http://localhost:5173'
-        dAppIconURL='https://walletconnect.puzzle.online/assets/logo_white-b85ba17c.png'
-      />
+      </div>
+
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import { BrowserRouter } from 'react-router-dom';
 import { PuzzleWalletProvider } from '@puzzlehq/sdk';
+import { PuzzleWeb3Modal } from '@puzzlehq/sdk';
 import "./index.css";
 
 const root = ReactDOM.createRoot(
@@ -16,5 +17,11 @@ root.render(
         <App />
       </BrowserRouter>
     </PuzzleWalletProvider>
+    <PuzzleWeb3Modal
+      dAppName='Puzzle Starter app'
+      dAppDescription="Let's Puzzle!"
+      dAppUrl='http://localhost:5173'
+      dAppIconURL='https://walletconnect.puzzle.online/assets/logo_white-b85ba17c.png'
+    />
   </React.StrictMode>
 );
