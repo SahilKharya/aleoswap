086889c2d5d6630d5399269180bb57540ece367f
Sahil Kharya <kharyasahil@gmail.com>
2023-08-29 13:49:22 -0700

    UI updates and SDK upgrade

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,33 +7,31 @@ function Header(props) {
   const { address, isConnected, connect } = props;
   return (
     <header>
-      <div className="leftH">
-        <Link to="/">
+      {isConnected && (
+        <div className="leftH">
+          <Link to="/">
 
-          <img src={Logo} alt="logo" className="logo" />
-        </Link>
-        {isConnected && (
+            <img src={Logo} alt="logo" className="logo" />
+          </Link>
           <Link to="/swap" className="link">
             <div className="headerItem">Swap</div>
-          </Link>)}
-          {isConnected && (
+          </Link>
           <Link to="/mint" className="link">
             <div className="headerItem">Mint</div>
-          </Link>)}
-          {/* {isConnected && (
-          <Link to="/create" className="link">
-            <div className="headerItem">Create</div>
-          </Link>)} */}
-      </div>
+          </Link>
+        </div>
+      )}
       <div className="rightH">
-        <div className="connectButton" onClick={connect}>
-          {isConnected ? (
+        {isConnected && (
+          <div className="connectButton" onClick={connect}>
+
             <>
               <span className="circle"></span>
               {address.address.slice(0, 6) + "..." + address.address.slice(55)}
             </>
-          ) : "Connect"}
-        </div>
+          </div>
+        )}
+
       </div>
     </header>
   );
